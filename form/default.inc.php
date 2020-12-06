26753822ef5ae59fcd3977d125f1e595b1c40b42
Steve Friedlander <s.friedlander@ufl.edu>
2020-12-06 17:55:45 -0500

    Added styling to contact page, added PHP forms

---
+++ form/default.inc.php
@@ -0,0 +1,52 @@
+<style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  header {
+    width: 100%;
+    background: #2f3941;
+    padding: 80px;
+    top: 0;
+    position: fixed;
+    z-index: 1;
+  }
+
+  nav {
+    width: 100%;
+    min-width: 600px;
+    max-width: 1800px;
+    text-align: right;
+  }
+
+  nav ul, nav ul li, nav ul li a {
+    display: inline;
+    list-style-type: none;
+    text-decoration: none;
+    color: white;
+    margin-right: 0px;
+    padding: 5px;
+    font-size: 18px;
+    font-weight: normal;
+    font-family: 'Commissioner', sans-serif;
+  }
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</li>
+      <li><a href="about.html">About</li>
+      <li><a href="portfolio.html">Portfolio</li>
+      <li><a href="contact.html">Contact</li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1> Oops! Something went wrong with your submission.</h1>
+  <p>Please try again!</p>
+</div>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,53 @@
+<style type="text/css">
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  header {
+    width: 100%;
+    background: #2f3941;
+    padding: 80px;
+    top: 0;
+    position: fixed;
+    z-index: 1;
+  }
+
+  nav {
+    width: 100%;
+    min-width: 600px;
+    max-width: 1800px;
+    text-align: right;
+  }
+
+  nav ul, nav ul li, nav ul li a {
+    display: inline;
+    list-style-type: none;
+    text-decoration: none;
+    color: white;
+    margin-right: 0px;
+    padding: 5px;
+    font-size: 18px;
+    font-weight: normal;
+    font-family: 'Commissioner', sans-serif;
+  }
+</style>
+
+<header>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</li>
+      <li><a href="about.html">About</li>
+      <li><a href="portfolio.html">Portfolio</li>
+      <li><a href="contact.html">Contact</li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Thank you for your inquiry</h1>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+  <p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+</div>
